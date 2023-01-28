e65c28ed19bbb38adf01c35623ea0f5afe4fae03
Malcolm <forster.malcolm@gmail.com>
2023-01-29 00:15:21 +1300

    Can  draw supports and begun on loads

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,11 @@
 var beamHeight;
 var beamThk;
 var canvasWidth;
+var canvasBorder = 50;
+
+var RollerSupports = [];
+var PinSupports = [];
+var FixedSupports = [];
 
 window.onload = function(){
 
@@ -12,16 +17,15 @@ window.onload = function(){
     beamThk = 10;
     ctx.lineWidth = beamThk;
     ctx.beginPath();
-    ctx.moveTo(50,beamHeight);
-    ctx.lineTo(canvasWidth-50,beamHeight);    
+    ctx.moveTo(canvasBorder,beamHeight);
+    ctx.lineTo(canvasWidth-canvasBorder,beamHeight);    
     ctx.stroke();
 
 };
 
 var ctrlDivs = ["loadDiv", "suppDiv", "beamDiv"];
 var supportDivs = ["RollerEdit", "PinnedEdit", "FixedEdit"];
-
-
+var loadDivs = ["DistributedEdit", "PointEdit", "MomentEdit"];
 var allDivsToHide = [...ctrlDivs, ...supportDivs];
 
 function hideOtherDivs(eleNum, array) {
@@ -32,7 +36,7 @@ function hideOtherDivs(eleNum, array) {
     while(i < array.length) {
         var otherEle = document.getElementById(array[i]);
         if(i != eleNum) {        
-            otherEle.style = "hidden";            
+            otherEle.style = "hidden";         
         } else {            
             otherEle.style.display = "block";
         }
@@ -46,7 +50,7 @@ function addElement(eleType) {
 
     if(eleType == 0) {
         //add a load to the beam
-        console.log("going to add a load");   
+        console.log("going to add a load");
     } else if(eleType == 1) {
         //add a connecton to the beam
         console.log("going to add a support");
@@ -96,16 +100,27 @@ function createSupport(c, kind, location) {
     //console.log("Creating a " + kind + " at location " + location.toString());
     if(kind==supportDivs[0]) {
         let rs = new RollerSupport(location, canvas);
+        RollerSupports.push(rs);
+        console.log(RollerSupports.length);
     } else if (kind == supportDivs[1]) {
         let ps = new PinSupport(location, canvas);
+        PinSupports.push(ps);
+        console.log(PinSupports.length);
     } else if (kind == supportDivs[2]) {
         let fs = new FixedSupport(location, canvas);
-    }
-    
+        
+        if(fs["side"] != 'f') {
+            FixedSupports.push(fs);
+            console.log(FixedSupports.length);
+        }
+    }    
 }
+
+// Below are the support classes
 
 class RollerSupport {
     constructor(location, canvas) {
+        this.location = location;
         var baseWidth = 20;
         var ballRad = 5;
         this.location = location;
@@ -118,20 +133,111 @@ class RollerSupport {
 
 class PinSupport {
     constructor(location, canvas) {
-        
+        this.location = location;
+        var sideLength = 10;
+        var bottomOfBeam = beamHeight +(beamThk/2)
+
+        canvas.beginPath();
+        canvas.moveTo(location, bottomOfBeam);
+        canvas.lineTo(location+(sideLength/2), bottomOfBeam+10);
+        canvas.lineTo(location-(sideLength/2), bottomOfBeam+10);
+        canvas.fill();        
     }    
 }
 
+
 class FixedSupport {
     constructor(location, canvas) {
+        var supportWidth = 20;
+        var supportHeight = 40;
         var side;
-        if(location < canvasWidth/2) {
-            side = 'r';
-        } else if(location >= canvasWidth/2) {
-            side = 'l';
-        }
 
-        canvas.beginPath();
-        canvas.rect();
+        this.side = 'f';
+
+        if(location < canvasWidth/2 && this.checkExists('l') == false) {
+            var xLoc = canvasBorder-supportWidth;
+            this.side = 'l';
+        } else if(location >= canvasWidth/2 && this.checkExists('r') == false) {
+            var xLoc = canvasWidth-canvasBorder;
+            this.side = 'r';
+        }
+        
+        if(side != 'f') {
+            this.location = this.side;
+            canvas.beginPath();
+            canvas.rect(xLoc,beamHeight-(supportHeight/2),supportWidth,supportHeight);
+            canvas.fill();
+        }
+    }
+
+    checkExists(checkSide) {
+        var found = false;
+        FixedSupports.forEach(element => {
+            if(element.side == checkSide) {
+                found = true;
+            }
+        });
+        return found;
     }
 }
+
+
+// Method called from html page to create loads
+
+function createLoad(c,kind,location,amount) {
+
+    var canvas = c.getContext("2d");
+    if(kind==loadDivs[0]) {
+        let rs = new DistributedLoad(location[0],location[1], amount[0],amount[1],canvas);
+        DistributedLoads.push(rs);
+        console.log(DistributedLoads.length);
+
+    } else if (kind == loadDivs[1]) {
+        let pl = new PointLoad(location, amount, canvas);
+        PointLoads.push(pl);
+        console.log(PointLoads.length);
+
+    } else if (kind == loadDivs[2]) {
+        let mo = new Moment(location, amount, canvas);
+        MomentLoads.push(mo);
+        console.log(MomentLoads.length);
+    }
+     
+}
+
+// Below are the load classes
+
+class DistributedLoad {
+    constructor(startLoc, finishLoc, startHeight, finishHeight, canvas) {
+        this.startLoc = startLoc;
+        this.finishLoc = finishLoc;
+        this.leftAmount = startHeight;
+        this.rightAmount = finishHeight;
+
+
+    }
+}
+
+class PointLoad {
+    constructor(location, amount, canvas) {
+        this.location = location;
+        this.amount = amount; // size of the point load applied
+
+        var sideLength = 10;
+        var topOfBeam = beamHeight - (beamThk/2)
+        
+        canvas.beginPath();
+        canvas.moveTo(location, topOfBeam);
+        canvas.lineTo(location+(sideLength/2), topOfBeam-10);
+        canvas.lineTo(location-(sideLength/2), topOfBeam-10);
+        canvas.fill();
+    }
+}
+
+class Moment {
+    constructor(location, amount, canvas) {
+        this.location = location;
+        this.amount = amount; // Size of the moment, +ve = cw and -ve = ccw moments
+    }
+}
+
